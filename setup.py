e1129ecc472ba90450ed9f89fa9d58782a6f157c
Trung Huynh <trung@playground.(none)>
2012-12-04 17:40:01 +0000

    Changed package name to be more PEP8

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ except:
     from distutils.core import setup
 
 
-setup(name='deeplearning',
+setup(name='deep_learning',
       version='0.1',
       description='Lisa Lab Deep Learning Networks',
       long_description=open('README.rst').read(),
       author='Lisa Lab',
       url='https://github.com/trunghlt/DeepLearningTutorials',
-      packages=['deeplearning'],
-      package_dir={'deeplearning':'code'},
-      provides=['deeplearning'],
+      packages=['deep_learning'],
+      package_dir={'deep_learning':'code'},
+      provides=['deep_learning'],
       license='Unknown',
       install_requires=['theano'])
